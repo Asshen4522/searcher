a169a562de021b9fc92f72b3ce83d71b850d6192
stpropan <Asshen4522@yandex.ru>
2024-11-19 18:03:06 +0300

    Текст

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const items = ["Первый-1", "Первый-2", "Второй-3", "Второ
 const vvod = document.getElementById('vvod')
 const vyvod = document.getElementById('vyvod')
 
+
 vvod.addEventListener('input', () => {
     vyvod.innerHTML = ""
     items.forEach(item => {
         if (item.toLowerCase().includes(vvod.value.toLowerCase())) {
             const li = document.createElement('li');
-            li.textContent = item;
+            li.innerText = item;
             vyvod.appendChild(li)
         }
     })
