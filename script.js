705a8f4f361bfd1cde1611f68c820fb9190b1116
stpropan <Asshen4522@yandex.ru>
2024-11-19 17:01:09 +0300

    Поисковик

---
+++ script.js
@@ -0,0 +1,15 @@
+const items = ["Первый-1", "Первый-2", "Второй-3", "Второй-4", "Второй-5", "Третий-6"]
+
+const vvod = document.getElementById('vvod')
+const vyvod = document.getElementById('vyvod')
+
+vvod.addEventListener('input', () => {
+    vyvod.innerHTML = ""
+    items.forEach(item => {
+        if (item.toLowerCase().includes(vvod.value.toLowerCase())) {
+            const li = document.createElement('li');
+            li.textContent = item;
+            vyvod.appendChild(li)
+        }
+    })
+})
